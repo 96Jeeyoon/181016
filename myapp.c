e032b5f71009e5726e16605a2748fb99ce35bfa3
Jeeyoon <baek7922@naver.com>
2018-10-16 01:02:10 -0700

    add pre/post process

--- myapp.c
+++ myapp.c
@@ -2,10 +2,25 @@
 #include "func2.h"
 #include <stdio.h>
 
+void preprocess()
+{
+	printf("thhththth");
+}
+
+void postprocess()
+{
+	printf("eretererer");
+}
+
+
 int main(void)
 {
+	preprocess();
+
 	func1();
 	func2();
+
+	postprocess();
 
 	return 0;
 }
